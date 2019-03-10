92b101b963d97a7e5335cb2a4e430715858350c5
KorolyovAlex <korolev.prostak@gmail.com>
2019-03-10 16:50:42 +0300

    Task is ready

---
+++ HW1/HW1/CodeEnter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HW1
+{
+    class CodeEnter
+    {
+        static void Main(string[] args)
+        {
+            if (args[0].Length < 2) // Line length check
+            {
+                Console.WriteLine("Error: not enough symbols in line");
+                return;
+            }
+            Sublines.PrintSublines(args[0]);    // Takes only first argument
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HW1/HW1/Sublines.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HW1
+{
+    public class Sublines
+    {
+        public static void PrintSublines(string line)
+        {
+            line.ToLower();
+            Console.WriteLine("Line: " + line);
+            Console.WriteLine("Sublines: ");
+            for (int index = 0; index < line.Length; index++)
+            {
+                for (int j = index + 1; j < line.Length; j++)
+                {
+                    if (line[j - 1] != line[j])
+                    {
+                        Console.WriteLine(line.Substring(index, j - index + 1));
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
