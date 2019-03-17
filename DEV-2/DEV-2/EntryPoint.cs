2f1d328bb3729be835eedba484634fc62766f7da
KorolyovAlex <korolev.prostak@gmail.com>
2019-03-17 23:19:30 +0300

    Perezaliv

---
+++ DEV-2/DEV-2/EntryPoint.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace DEV_2
+{
+    class EntryPoint
+    {
+        private static bool LetterCheck(string word)
+        {
+            bool plus = false;
+            foreach (char letter in word)
+            {
+                if (letter < 192 && letter != '+')
+                {
+                    Console.WriteLine("Error: use of invalid symbol in line " + word);
+                    Console.WriteLine("Only '+' and russian lowercase letters are allowed");
+                    return true;
+                }
+                if (letter == '+')
+                {
+                    if (plus)
+                    {
+                        Console.WriteLine("Error: too many '+' in line " + word);
+                        return true;
+                    }
+                    plus = true;
+                }
+            }
+            if (plus)
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Error: there is no '+' in line " + word);
+                return true;
+            }
+        }
+
+        static int Main(string[] args)
+        {
+            foreach (string word in args)
+            {
+                if (LetterCheck(word))
+                {
+                    continue;
+                }
+                var transcription = new WordToTranscription(word);
+                Console.WriteLine("Фонема слова " + word + ": " + transcription.Translate());
+            }
+            return 0;
+        }
+    }
+}
