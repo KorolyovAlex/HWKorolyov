852e3df39a4f9a5e594e5c3f22fa6825780455b4
KorolyovAlex <korolev.prostak@gmail.com>
2019-04-10 18:02:45 +0300

    Initial commit

---
+++ DEV-6/DEV-6/CountTypesCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_6
+{
+    class CountTypesCommand
+    {
+        private List<Brand> carList;
+
+        public CountTypesCommand(List<Brand> carList)
+        {
+            this.carList = carList;
+        }
+    }
+}
